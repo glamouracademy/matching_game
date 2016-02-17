6e48210b60ae50bc3452b44a911926914b39b2d4
Charlotte Chang <yellowspiderlove@gmail.com>
2016-02-17 14:39:20 -0500

    add color randomizer for option

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ var rgbColor;
 var piece;
 var answer;
 
-var correctCounter = 0;
-var incorrectCounter = 0;
-
+//sets the piece color for the markup
 function setPieceColor(rgbColor) {
 	var outputDiv = document.getElementById('original');	
 	outputDiv.style.backgroundColor = rgbColor;
@@ -14,10 +12,10 @@ function setPieceColor(rgbColor) {
 function addPiece(piece) {
 	var outputChoices = document.getElementById('options');
 	outputChoices.appendChild(piece);
-	piece.style.backgroundColor = rgbColor;
+	piece.style.backgroundColor = rgbColor2;
 }
 
-//create random color
+//create random color for original
 function createRandomColor() {
 	var red = Math.floor(Math.random() * 256 );
 	var green = Math.floor(Math.random() * 256 );
@@ -25,61 +23,53 @@ function createRandomColor() {
 	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';	
 }
 
-//create a piece
+//create random color for option
+function createRandomOptionColor() {
+	var red = Math.floor(Math.random() * 256 );
+	var green = Math.floor(Math.random() * 256 );
+	var blue = Math.floor(Math.random() * 256 );
+	rgbColor2 = 'rgb(' + red + ',' + green + ',' + blue + ')';	
+}
+
+//create a piece div
 function createDiv() {
 	piece = document.createElement("div");
 	piece.className = "piece";
 }
 
-
-// function validateForm() {
-//     var answer = document.forms["myForm"]["answer"].value;
-    
-//     answer = answer.toUpperCase();
-//     console.log("I'm here!");
-//     console.log(answer);
-
-//     if (answer == null || answer == "") {
-//         alert("Take a guess!");
-//         return false;
-    		
-//     } else if (answer === "Y") {
-//     		// correctCounter += 1;
-//   			document.write("Nice one!");
-//   			console.log("Made it in!");
-//   			// console.log(correctCounter);
-//   			return true;
-
-//     } else {
-//     		// incorrectCounter += 1;
-//     		return true;
-//     }
-// }
-	
-
+//board set up
 createRandomColor();
+createRandomOptionColor();
 createDiv();
 setPieceColor(rgbColor);
 addPiece(piece);
 
 
-//ask user for input
-while (true) {
-	answer = prompt("Do the pieces match? Type Y or N.")
-	answer = answer.toUpperCase();
-	if (answer === 'Y') {
-		correctCounter += 1;
-		getTally();
-		break;
-	} else {
-		incorrectCounter += 1;
-		getTally();
-		break;
-	}
-}
 
 
-//display tally
-function getTally() {
-	document.write("You got " + correctCounter + " correct and " + incorrectCounter + " incorrect.");
-}
+
+// var correctCounter = 0;
+// var incorrectCounter = 0;
+
+
+// //ask user for input
+// while (true) {
+// 	answer = prompt("Do the pieces match? Type Y or N.")
+// 	answer = answer.toUpperCase();
+// 	if (answer === 'Y') {
+// 		correctCounter += 1;
+// 		getTally(correctCounter);
+// 		break;
+// 	} else {
+// 		incorrectCounter += 1;
+// 		getTally();
+// 		break;
+// 	}
+// }
+
+// function getTally() {
+// 	document.write("<p>You got " + correctCounter + " correct and " + incorrectCounter + " incorrect.</p>");
+// }
+
+
+
