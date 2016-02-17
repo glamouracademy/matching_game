74e02863f2b114776d0142ad0e7bc62459965c80
Charlotte Chang <yellowspiderlove@gmail.com>
2016-02-17 11:19:02 -0500

    add option piece with same color as original

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var green;
 var blue;
 var rgbColor;
 
-function print(rgbColor) {
+var piece;
+
+function setPieceColor(rgbColor) {
 	var outputDiv = document.getElementById('original');	
 	outputDiv.style.backgroundColor = rgbColor;
-
 }
+
+//adds piece to options and set background color of piece
+function addPiece(piece) {
+	var outputChoices = document.getElementById('options');
+	outputChoices.appendChild(piece);
+	piece.style.backgroundColor = rgbColor;
+}
+
 
 red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
@@ -15,4 +24,9 @@ blue = Math.floor(Math.random() * 256 );
 rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 
 
-print(rgbColor);
+piece = document.createElement("div");
+piece.className = "piece";
+
+
+setPieceColor(rgbColor);
+addPiece(piece);
