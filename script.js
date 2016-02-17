144036e68989e9da0161acaa75374ed0b9baf2a1
Charlotte Chang <yellowspiderlove@gmail.com>
2016-02-17 10:46:11 -0500

    add working color randomizer to original piece

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var red;
+var green;
+var blue;
+var rgbColor;
 
+function print(rgbColor) {
+	var outputDiv = document.getElementById('original');	
+	outputDiv.style.backgroundColor = rgbColor;
+
+}
+
+red = Math.floor(Math.random() * 256 );
+green = Math.floor(Math.random() * 256 );
+blue = Math.floor(Math.random() * 256 );
+rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+
+print(rgbColor);
