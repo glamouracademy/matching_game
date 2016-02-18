e8a4ad54931bdd316b01c52f228710a0a1cee3d2
Charlotte Chang <yellowspiderlove@gmail.com>
2016-02-18 09:41:48 -0500

    dry up duplicate color randomizer and add color array

--- script.js
+++ script.js
@@ -4,35 +4,48 @@ var piece;
 var answer;
 var correctCounter = 0;
 var incorrectCounter = 0;
+var colorOptions = [];
 
 //sets piece color
 function setPieceColor(rgbColor) {
 	var outputDiv = document.getElementById('original');	
-	outputDiv.style.backgroundColor = rgbColor;
+	outputDiv.style.backgroundColor = colorOptions[0];
 }
 
 //adds piece to options and set background color of piece
 function addPiece(piece) {
 	var outputChoices = document.getElementById('options');
 	outputChoices.appendChild(piece);
-	piece.style.backgroundColor = rgbColor2;
+	piece.style.backgroundColor = colorOptions[1];
 }
 
-//create random color
+
+//loop through creating random colors
 function createRandomColor() {
 	var red = Math.floor(Math.random() * 256 );
 	var green = Math.floor(Math.random() * 256 );
 	var blue = Math.floor(Math.random() * 256 );
 	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';	
+	colorOptions.push(rgbColor);
 }
 
-//create random color
-function createRandomColor2() {
-	var red = Math.floor(Math.random() * 256 );
-	var green = Math.floor(Math.random() * 256 );
-	var blue = Math.floor(Math.random() * 256 );
-	rgbColor2 = 'rgb(' + red + ',' + green + ',' + blue + ')';	
-}
+
+// //create random color
+// function createRandomColor() {
+// 	var red = Math.floor(Math.random() * 256 );
+// 	var green = Math.floor(Math.random() * 256 );
+// 	var blue = Math.floor(Math.random() * 256 );
+// 	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';	
+// }
+
+// //create random color
+// function createRandomColor2() {
+// 	var red = Math.floor(Math.random() * 256 );
+// 	var green = Math.floor(Math.random() * 256 );
+// 	var blue = Math.floor(Math.random() * 256 );
+// 	rgbColor2 = 'rgb(' + red + ',' + green + ',' + blue + ')';	
+// }
+
 
 //create a piece
 function createDiv() {
@@ -45,11 +58,11 @@ window.onload = function () {
 	answer = prompt("Do the pieces match? Type Y or N.");
 	answer = answer.toUpperCase();
 	
-	if (rgbColor === rgbColor2 && answer === 'Y') {
+	if (colorOptions.includes(rgbColor) && answer === 'Y') {
 		correctCounter += 1;
 		getTally(correctCounter);
 		
-	} else if (rgbColor != rgbColor2 && answer === 'N') {
+	} else if (colorOptions.includes(rgbColor) === false && answer === 'N') {
 		correctCounter += 1;
 		getTally();
 		
@@ -61,7 +74,6 @@ window.onload = function () {
 
 
 //get results by id
-
 function getTally() {
 	var tally = document.getElementById("results");
 	tallyDiv = "<p>You got " + correctCounter + " correct and " + incorrectCounter + " incorrect.</p>";
@@ -71,7 +83,7 @@ function getTally() {
 
 
 createRandomColor();
-createRandomColor2();
+createRandomColor();
 createDiv();
 setPieceColor(rgbColor);
 addPiece(piece);
